a1ddf02765b4eb4f289466f65be5f416b20b4ee5
gjcarter <cart8282@vandals.uidaho.edu>
2018-05-02 18:48:01 -0700

    Fixed failed solutions and general movement
    Fixed failed solutions found during EXPO testing. Refined movement so it feels better and the user is able to access more positions with the Leap. Joint three now locked in place to prevent confusion with hand roll. Hand roll controls joint five. Edited launch file to include starting iksolver and Leap sensor_sender nodes. Added script to launch all ROS nodes and leap driver and keep them running.

--- src/iksolver.cpp
+++ src/iksolver.cpp
@@ -23,7 +23,7 @@ std::vector<double> joint_values;
 /* COORDINATE INFO 
  * Top coord (x,y) = (.2, 0.48)
  * Bottom coord (x,y) =  (0.2, 0.06)
- * Extended coord (x,y) = (.38, .2)
+ * Extended coord (x,y) = (.37, .2)
  * Pulled back coord (x,y) = (.19, .2)
  * Z limits = +-.3
  */
@@ -34,32 +34,38 @@ std::vector<double> joint_values;
  */
 
 void calculateIK(leap_motion::leapros msg){
+
+  //Temporary valid orientation so that iksolver doesn't fail
   pose.orientation.x = 0;
   pose.orientation.y = -0.71;
   pose.orientation.z = .0;
   pose.orientation.w = 0.7;
-  pose.position.y = msg.palmpos.x/250 * .3;
+
+  pose.position.y = msg.palmpos.x/170 * .3;
   /* Currently limiting x to a plane. Could be switched to another 
    * ellipsoid in the future.
    */
   if(msg.palmpos.z <= 0)
-    pose.position.x =  ((abs(msg.palmpos.z) * .09)/150 + .29);
+    pose.position.x =  ((abs(msg.palmpos.z) * .09)/250 + .27);
   else if (msg.palmpos.z > 0)
-    pose.position.x = (.29 - (msg.palmpos.z * .09)/150);
-  pose.position.z = msg.palmpos.y/350 * .40;
+    pose.position.x = (.27 - (msg.palmpos.z * .09)/130);
+  pose.position.z = (msg.palmpos.y-50)/350 * .42;
 
   /* Using equation for an ellipsoid to check if pose it within 3 given radii
    * Equation: x^2/radius1^2 + y^2/radius2^2 + z^2/radius3^2 = 1
    * radius1 = .18, radius2 = .2, radius3 = .3
    */
-  if ((((pose.position.x - .2) * (pose.position.x - .2) / .0324) +
+  if ((((pose.position.x - .18) * (pose.position.x - .18) / .0324) +
        (pose.position.y * pose.position.y / .09) +
-       ((pose.position.z - .2) * (pose.position.z -.2) / .04)) <= 1) {
+       ((pose.position.z - .21) * (pose.position.z - .21) / .0441)) <= 1) {
     //X needs to be negative to be a valid pose
     pose.position.x *= -1;
     bool found_ik = discoverbot_state->setFromIK(model_group, pose, 3, 0.1);
     if(!found_ik){
-      //std::cout << "Failed IK Solution" << std::endl;
+      /*std::cout << "Failed IK Solution" << std::endl;
+      std::cout << "x:" <<  pose.position.x << std::endl;
+      std::cout << "z:" << pose.position.y << std::endl;
+      std::cout << "y:" << pose.position.z << std::endl;*/
       return;
     }
     //else
@@ -75,18 +81,19 @@ void calculateIK(leap_motion::leapros msg){
      */ 
     if(msg.ypr.x > 0)
       msg.ypr.x *= -1;
-    goal.position[4] = (msg.ypr.x/1.5 + 45)/57.2958;
-    //std::cout << "yaw: " << msg.ypr.x << std::endl;
-    goal.position[5] = msg.ypr.z/57.2958; 
+    goal.position[4] = (msg.ypr.x/1.2 + 45)/57.2958;
+    /* Using roll of hand to change angle of joint 3 
+     * is preferred but needs physical testing on robot
+     * in order to see if it would be valid with some random
+     * movements due to Leap's rpy tracking not being as
+     * accurate as xyz. For now just moving joint 5 with
+     * hand roll.
+     */
+    goal.position[5] = (msg.ypr.z)/-57.2958;
+    goal.position[3] = 0;
+    //goal.position[3] = (msg.ypr.z)/-57.2958;
     pub.publish(goal);
-    
-    /*std::cout << "x:" <<  pose.position.x << std::endl;
-    std::cout << "z:" << pose.position.y << std::endl;
-    std::cout << "y:" << pose.position.z << std::endl;
-    std::cout << "orientation.x " << pose.pose.orientation.x << std::endl;
-    std::cout << "orientation.y " << pose.pose.orientation.y << std::endl;
-    std::cout << "orientation.z " << pose.pose.orientation.z << std::endl;
-    std::cout << "orientation.w " << pose.pose.orientation.w << std::endl;*/
+
     
   }   
 }
@@ -94,6 +101,9 @@ void calculateIK(leap_motion::leapros msg){
 int main(int argc, char **argv){
   ros::init(argc, argv, "discoverbot_ik");
   ros::NodeHandle node;
+  /* Currently publishing to joint_states for Rviz but when Arduino
+   * is implemented this will publish to that
+   */
   pub = node.advertise<sensor_msgs::JointState>("joint_states", 1000);
 							      
   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
@@ -105,16 +115,6 @@ int main(int argc, char **argv){
   spinner.start();
   group = new moveit::planning_interface::MoveGroupInterface("manipulator");
   
-  /*geometry_msgs::PoseStamped pose_target;
-  pose_target = group->getCurrentPose();
-  std::cout << "position.x: " << pose_target.pose.position.x << std::endl;
-  std::cout << "position.y: " << pose_target.pose.position.y << std::endl;
-  std::cout << "position.z: " << pose_target.pose.position.z << std::endl;
-  std::cout << "orientation.x " << pose_target.pose.orientation.x << std::endl;
-  std::cout << "orientation.y " << pose_target.pose.orientation.y << std::endl;
-  std::cout << "orientation.z " << pose_target.pose.orientation.z << std::endl;
-  std::cout << "orientation.w " << pose_target.pose.orientation.w << std::endl;*/
-
   ros::Subscriber sub = node.subscribe("leapmotion/data", 1000, calculateIK);
   
   ros::spin();
